d19035db82f6c3246bad6af4e973799c28db19ac
liaosl <liaosl@boqii.com>
2016-08-22 16:24:17 +0800

    btn title view与imgView的几种情况

--- dolin_demo/Dolin1/LocalNotificationViewController.m
+++ dolin_demo/Dolin1/LocalNotificationViewController.m
@@ -51,19 +51,49 @@
 
 - (UIButton*)btn {
     if (!_btn) {
-        CGFloat btnWidth = 40.0;
-        CGFloat btnHeight = 40.0 + 13.0;
         _btn = [UIButton buttonWithType:UIButtonTypeCustom];
-        _btn.frame = CGRectMake(0, 0,btnWidth,btnHeight);
+        _btn.frame = CGRectMake(0, 0,300,300);
         _btn.center = CGPointMake(SCREEN_WIDTH / 2, SCREEN_HEIGHT_NOT_NAVIGATIONBAR_AND_TABBAR / 2);
-        [_btn setImage:[UIImage imageNamed:@"btn_home_test"] forState:UIControlStateNormal];
+        [_btn setImage:[UIImage imageNamed:@"btn_like"] forState:UIControlStateNormal];
         [_btn setTitle:@"test" forState:UIControlStateNormal];
         [_btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         [_btn setTitleColor:[UIColor redColor] forState:UIControlStateHighlighted];
         
-        _btn.backgroundColor = RANDOM_UICOLOR;
-        _btn.imageEdgeInsets = UIEdgeInsetsMake(-5, 0, 0, 0); // img往上偏移5
-        _btn.titleEdgeInsets = UIEdgeInsetsMake(40, -40, 0, 0); // title 往下偏移40 往左偏移40
+        _btn.backgroundColor = [UIColor cyanColor];
+        
+        // 图片在左边，全部居中
+        CGFloat spacing = 20;
+        CGSize imageSize;
+        CGSize titleSize;
+        
+        
+//        imageSize = _btn.imageView.frame.size;
+//        _btn.titleEdgeInsets = UIEdgeInsetsMake(0.0,  0.0, 0.0,  - spacing/2);
+//        // 对titleEdgeInsets的属性赋值之后，title的宽度可能会变化，所以我们需要在titleEdgeInsets赋值之后去获取titleSize
+//        titleSize = _btn.titleLabel.frame.size;
+//        _btn.imageEdgeInsets = UIEdgeInsetsMake(0.0, - spacing/2, 0.0, 0.0);
+        
+//         图片在上边，全部居中
+        imageSize = _btn.imageView.frame.size;
+        titleSize = _btn.titleLabel.frame.size;
+        spacing = 2;
+        _btn.titleEdgeInsets = UIEdgeInsetsMake(0.0, - imageSize.width, - (imageSize.height + spacing/2), 0.0);
+        titleSize = _btn.titleLabel.frame.size;
+        _btn.imageEdgeInsets = UIEdgeInsetsMake(- (titleSize.height + spacing/2), 0.0, 0.0, - titleSize.width);
+        
+        // 图片在右边，全部居中
+//        spacing = 20;
+//        imageSize = _btn.imageView.frame.size;
+//        _btn.titleEdgeInsets = UIEdgeInsetsMake(0.0, - imageSize.width, 0.0, imageSize.width + spacing/2);
+//        titleSize = _btn.titleLabel.frame.size;
+//        _btn.imageEdgeInsets = UIEdgeInsetsMake(0.0, titleSize.width + spacing/2, 0.0, - titleSize.width);
+//
+        // 图片在右边，文字居中
+//        imageSize = _btn.imageView.frame.size;
+//        _btn.titleEdgeInsets = UIEdgeInsetsMake(0.0, - imageSize.width, 0.0, 0.0);
+//        titleSize = _btn.titleLabel.frame.size;
+//        _btn.imageEdgeInsets = UIEdgeInsetsMake(0.0, titleSize.width + imageSize.width + spacing, 0.0, - titleSize.width);
+        
         
         [_btn addTarget:self action:@selector(btnAction) forControlEvents:UIControlEventTouchUpInside];
     }
