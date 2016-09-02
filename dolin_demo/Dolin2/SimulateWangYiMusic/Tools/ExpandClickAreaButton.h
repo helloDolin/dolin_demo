3a6d8d66daa1f947dfa902cace946ea83e77900c
liaosl <liaosl@boqii.com>
2016-09-02 16:24:15 +0800

    1

---
+++ dolin_demo/Dolin2/SimulateWangYiMusic/Tools/ExpandClickAreaButton.h
@@ -0,0 +1,13 @@
+//
+//  ExpandClickAreaButton.h
+//  dolin_demo
+//
+//  Created by dolin on 16/8/29.
+//  Copyright © 2016年 shaolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ExpandClickAreaButton : UIButton
+
+@end
