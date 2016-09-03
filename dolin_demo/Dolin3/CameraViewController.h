1a11fcf439f2e6e2a74074c4ce0810d76b03278a
liaoshaolim <366688603@qq.com>
2016-09-04 02:41:29 +0800

    camera study

---
+++ dolin_demo/Dolin3/CameraViewController.h
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.h
+//  dolin_demo
+//
+//  Created by shaolin on 16/9/4.
+//  Copyright © 2016年 shaolin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CameraViewController : UIViewController
+
+@end
