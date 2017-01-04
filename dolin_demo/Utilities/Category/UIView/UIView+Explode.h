54a29b75f55f980c79f2efaa9bce2f2b06157011
liaoshaolim <366688603@qq.com>
2017-01-04 17:42:57 +0800

    + DLButton + 碎片化view并移除动画的分类

---
+++ dolin_demo/Utilities/Category/UIView/UIView+Explode.h
@@ -0,0 +1,26 @@
+//
+//  UIView+CoreAnimation.h
+//  CoreAnimationPlayGround
+//
+//  Created by Daniel Tavares on 27/03/2013.
+//  Copyright (c) 2013 Daniel Tavares. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <QuartzCore/QuartzCore.h>
+#import <objc/runtime.h>
+#import <Foundation/Foundation.h>
+
+typedef void(^ExplodeCompletion)(void);
+
+/**
+ *  使view碎片化的动画
+ */
+@interface UIView (Explode)
+
+@property (nonatomic, copy) ExplodeCompletion completionCallback;
+
+- (void)lp_explode;
+- (void)lp_explodeWithCallback:(ExplodeCompletion)callback;
+
+@end
