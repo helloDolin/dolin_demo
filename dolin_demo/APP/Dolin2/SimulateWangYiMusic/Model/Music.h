a288fff30ea4cdef978a104e94c01baef30d2225
liaoshaolim <366688603@qq.com>
2017-01-23 17:10:30 +0800

    1

--- dolin_demo/APP/Dolin2/SimulateWangYiMusic/Model/Music.h
+++ dolin_demo/APP/Dolin2/SimulateWangYiMusic/Model/Music.h
@@ -10,17 +10,17 @@
 
 @interface Music : NSObject
 
-@property (nonatomic,strong) NSString *mp3Url;  //歌曲网址
-@property (nonatomic,strong) NSString *picUrl;  //图片网址
-@property (nonatomic,strong) NSString *name;    //歌名
-@property (nonatomic,strong) NSString *singer;  //歌手
-@property (nonatomic,strong) NSString *lyric;   //歌词
-@property (nonatomic,strong) NSString *theAlbumName;//专辑名
-@property (nonatomic,strong) NSString *musicIntroduce;//歌曲简介
-@property (nonatomic,strong) NSString *musicID;//歌曲ID
-@property (nonatomic,strong) NSString *musicDownloadID;//歌曲下载ID
-@property (nonatomic,strong) NSString *listTheCoverUrl;//歌单封面
-@property (nonatomic,strong) NSString *listName;//歌单名称
-@property (nonatomic,strong) NSString *musicListCount;//歌单歌曲个数
+@property (nonatomic,copy) NSString *mp3Url;            //歌曲网址
+@property (nonatomic,copy) NSString *picUrl;            //图片网址
+@property (nonatomic,copy) NSString *name;              //歌名
+@property (nonatomic,copy) NSString *singer;            //歌手
+@property (nonatomic,copy) NSString *lyric;             //歌词
+@property (nonatomic,copy) NSString *theAlbumName;      //专辑名
+@property (nonatomic,copy) NSString *musicIntroduce;    //歌曲简介
+@property (nonatomic,copy) NSString *musicID;           //歌曲ID
+@property (nonatomic,copy) NSString *musicDownloadID;   //歌曲下载ID
+@property (nonatomic,copy) NSString *listTheCoverUrl;   //歌单封面
+@property (nonatomic,copy) NSString *listName;          //歌单名称
+@property (nonatomic,copy) NSString *musicListCount;    //歌单歌曲个数
 
 @end
