a6720393ba3d0579f1e11ddc56b0fd55b8be1c41
liaoshaolim <366688603@qq.com>
2017-03-13 16:35:58 +0800

    WK学习

--- dolin_demo/APP/Dolin1/WebViewVC/TestWKWebViewVC.m
+++ dolin_demo/APP/Dolin1/WebViewVC/TestWKWebViewVC.m
@@ -9,7 +9,13 @@
 #import "TestWKWebViewVC.h"
 #import <WebKit/WebKit.h>
 
-@interface TestWKWebViewVC ()<WKUIDelegate,WKNavigationDelegate>
+// WKNavigationDelegate主要处理一些跳转、加载处理操作，
+// WKUIDelegate主要处理JS脚本，确认框，警告框等。因此WKNavigationDelegate更加常用。
+
+@interface TestWKWebViewVC ()<WKUIDelegate,WKNavigationDelegate,WKScriptMessageHandler>
+{
+    WKUserContentController *_userContentController;
+}
 @property (nonatomic, strong) WKWebView *webView;
 @property (nonatomic, strong) UIProgressView *progressView;
 
@@ -19,45 +25,213 @@
 
 - (void)dealloc
 {
+    NSLog(@"%s",__func__);
+    [self.webView removeObserver:self forKeyPath:@"loading"];
+    [self.webView removeObserver:self forKeyPath:@"title"];
     [self.webView removeObserver:self forKeyPath:@"estimatedProgress"];
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
-
+    
     [self.view addSubview:self.webView];
     [self.view addSubview:self.progressView];
     
-    NSString* urlStr = @"http://liaoshaolim.github.io/";
-    [_webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlStr]]];
+    NSURL *path = [[NSBundle mainBundle] URLForResource:@"dolinH5" withExtension:@"html"];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:path]];
+//    NSString* urlStr = @"http://liaoshaolim.github.io/";
+//    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:urlStr]]];
+    
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"后退" style:UIBarButtonItemStyleDone target:self action:@selector(goback)];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"前进" style:UIBarButtonItemStyleDone target:self action:@selector(gofarward)];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    WKUserContentController *controller = self.webView.configuration.userContentController;
+    [controller removeScriptMessageHandlerForName:@"senderModel"];
+}
+
+- (void)goback {
+    if ([self.webView canGoBack]) {
+        [self.webView goBack];
+    }
+    else {
+        [self.navigationController popViewControllerAnimated:YES];
+    }
+}
+
+- (void)gofarward {
+    if ([self.webView canGoForward]) {
+        [self.webView goForward];
+    }
 }
 
 #pragma mark - KVO
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    
+    if ([keyPath isEqualToString:@"title"]) {
+        self.title = self.webView.title;
+    }
+    
+    else if ([keyPath isEqualToString:@"loading"]){
 
-- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context{
-    if ([keyPath isEqualToString:@"estimatedProgress"]) {
+    }
+    
+    else if ([keyPath isEqualToString:@"estimatedProgress"]){
+        // estimatedProgress取值范围是0-1;
         [UIView animateWithDuration:0.3 animations:^{
             [_progressView setProgress:_webView.estimatedProgress];
-            [_progressView layoutIfNeeded];
-            
         } completion:^(BOOL finished) {
             if (_progressView.progress == 1) {
                 [_progressView removeFromSuperview];
             };
-            
+        }];
+    }
+    
+    if (!self.webView.loading) {
+        [UIView animateWithDuration:0.5 animations:^{
+            self.progressView.alpha = 0;
         }];
     }
 }
 
+#pragma mark - WKNavigationDelegate
+// 页面开始加载时调用
+- (void)webView:(WKWebView *)webView didStartProvisionalNavigation:(WKNavigation *)navigation{
+}
+
+// 当内容开始返回时调用
+- (void)webView:(WKWebView *)webView didCommitNavigation:(WKNavigation *)navigation{
+}
+
+// 页面加载完成之后调用
+- (void)webView:(WKWebView *)webView didFinishNavigation:(WKNavigation *)navigation{
+}
+
+// 页面加载失败时调用
+- (void)webView:(WKWebView *)webView didFailProvisionalNavigation:(WKNavigation *)navigation{
+}
+
+// 接收到服务器跳转请求之后调用
+- (void)webView:(WKWebView *)webView didReceiveServerRedirectForProvisionalNavigation:(WKNavigation *)navigation{
+}
+
+// 在收到响应后，决定是否跳转
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationResponse:(WKNavigationResponse *)navigationResponse decisionHandler:(void (^)(WKNavigationResponsePolicy))decisionHandler{
+    
+    NSLog(@"%@",navigationResponse.response.URL.absoluteString);
+    //允许跳转
+    decisionHandler(WKNavigationResponsePolicyAllow);
+    //不允许跳转
+    //decisionHandler(WKNavigationResponsePolicyCancel);
+}
+
+// 在发送请求之前，决定是否跳转
+- (void)webView:(WKWebView *)webView decidePolicyForNavigationAction:(WKNavigationAction *)navigationAction decisionHandler:(void (^)(WKNavigationActionPolicy))decisionHandler {
+    
+    NSString *hostname = navigationAction.request.URL.host.lowercaseString;
+    NSLog(@"%@",hostname);
+    if (navigationAction.navigationType == WKNavigationTypeLinkActivated
+        && ![hostname containsString:@".baidu.com"]) {
+        // 对于跨域，需要手动跳转
+        [[UIApplication sharedApplication] openURL:navigationAction.request.URL];
+        
+        // 不允许web内跳转
+        decisionHandler(WKNavigationActionPolicyCancel);
+    } else {
+        self.progressView.alpha = 1.0;
+        decisionHandler(WKNavigationActionPolicyAllow);
+    }
+}
+
+#pragma mark - WKUIDelegate
+// 创建一个新的WebView
+- (WKWebView *)webView:(WKWebView *)webView createWebViewWithConfiguration:(WKWebViewConfiguration *)configuration forNavigationAction:(WKNavigationAction *)navigationAction windowFeatures:(WKWindowFeatures *)windowFeatures{
+    
+    return [[WKWebView alloc]init];
+}
+
+// 输入框
+- (void)webView:(WKWebView *)webView runJavaScriptTextInputPanelWithPrompt:(NSString *)prompt defaultText:(nullable NSString *)defaultText initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(NSString * __nullable result))completionHandler{
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"输入框" message:@"调用输入框" preferredStyle:UIAlertControllerStyleAlert];
+    [alert addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.textColor = [UIColor blackColor];
+    }];
+    
+    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler([[alert.textFields lastObject] text]);
+    }]];
+    
+    [self presentViewController:alert animated:YES completion:NULL];
+}
+
+// 确认框
+- (void)webView:(WKWebView *)webView runJavaScriptConfirmPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(BOOL result))completionHandler{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"确认框" message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"确定" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler(YES);
+    }]];
+    [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler(NO);
+    }]];
+    [self presentViewController:alert animated:YES completion:NULL];
+}
+
+// 警告框
+- (void)webView:(WKWebView *)webView runJavaScriptAlertPanelWithMessage:(NSString *)message initiatedByFrame:(WKFrameInfo *)frame completionHandler:(void (^)(void))completionHandler{
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"警告" message:message preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        completionHandler();
+    }]];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+#pragma mark - WKScriptMessageHandler
+- (void)userContentController:(WKUserContentController *)userContentController didReceiveScriptMessage:(WKScriptMessage *)message{
+    // 这里可以通过name处理多组交互
+    if ([message.name isEqualToString:@"senderModel"]) {
+        //body只支持NSNumber, NSString, NSDate, NSArray,NSDictionary 和 NSNull类型
+    }
+
+    NSLog(@"===========\nname:%@\nbody:%@\nframeInfo:%@\n===========",message.name,message.body,message.frameInfo);
+}
 
 #pragma mark - getter
 - (WKWebView *)webView{
     if (!_webView) {
-        _webView = [[WKWebView alloc]initWithFrame:CGRectMake(0, 64, SCREEN_WIDTH, SCREEN_HEIGHT - 64)];
+        WKWebViewConfiguration *config = [WKWebViewConfiguration new];
+        // 初始化偏好设置属性：preferences
+        config.preferences = [WKPreferences new];
+        // The minimum font size in points default is 0;
+        config.preferences.minimumFontSize = 30;
+        // 是否支持JavaScript
+        config.preferences.javaScriptEnabled = YES;
+        // 不通过用户交互，是否可以打开窗口
+        config.preferences.javaScriptCanOpenWindowsAutomatically = NO;
+        // 通过JS与webView内容交互
+        config.userContentController = [WKUserContentController new];
+        // 注入JS对象名称senderModel，当JS通过senderModel来调用时，我们可以在WKScriptMessageHandler代理中接收到
+        [config.userContentController addScriptMessageHandler:self name:@"senderModel"];
+
+        _webView = [[WKWebView alloc]initWithFrame:CGRectMake(0, 64, SCREEN_WIDTH, SCREEN_HEIGHT - 64) configuration:config];
+        
         _webView.UIDelegate = self;
         _webView.navigationDelegate = self;
-        [_webView addObserver:self forKeyPath:@"estimatedProgress" options:NSKeyValueObservingOptionNew context:nil];
+        
+        [_webView addObserver:self
+                       forKeyPath:@"loading"
+                          options:NSKeyValueObservingOptionNew
+                          context:nil];
+        [_webView addObserver:self
+                       forKeyPath:@"title"
+                          options:NSKeyValueObservingOptionNew
+                          context:nil];
+        [_webView addObserver:self
+                       forKeyPath:@"estimatedProgress"
+                          options:NSKeyValueObservingOptionNew
+                          context:nil];
     }
     return _webView;
 }
@@ -66,8 +240,7 @@
     if (!_progressView ) {
         _progressView = [[UIProgressView alloc]initWithFrame:CGRectMake(0, 90, SCREEN_WIDTH, 0)];
         _progressView.progressTintColor = RANDOM_UICOLOR;
-        _progressView.trackTintColor = [UIColor clearColor];
-        _progressView.progress = 0;
+        _progressView.trackTintColor = RANDOM_UICOLOR;//[UIColor clearColor];
     }
     return _progressView;
 }
