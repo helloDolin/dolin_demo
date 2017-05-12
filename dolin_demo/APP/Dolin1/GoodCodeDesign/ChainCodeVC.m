7a187738c0b65666b851a3b7e114b39114b3ca7a
liaoshaolim <366688603@qq.com>
2017-05-12 18:34:26 +0800

    .

--- dolin_demo/APP/Dolin1/GoodCodeDesign/ChainCodeVC.m
+++ dolin_demo/APP/Dolin1/GoodCodeDesign/ChainCodeVC.m
@@ -29,6 +29,8 @@
         make.add(10).muilt(10).sub(10).divide(3);
     }];
     
+    NSLog(@"%ld",result);
+    
     [BlockAlertView alertWithTitle:@"仿Masonry设计思路" message:@"仿Masonry设计思路\n调用可以看这个类里的一个例子，原理很简单\n但是设计思路很棒！" cancelBtnWithTitle:nil cancelBlock:nil confirmButtonWithTitle:@"yeah！" confirmBlock:nil];
 }
 
