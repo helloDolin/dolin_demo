e8bf12f2ea4778c3f708f362d4412f68e77133a6
liaoshaolim <366688603@qq.com>
2019-10-11 14:21:33 +0800

    study 1.有状态无状态widget 2.build函数 3.state 4.路由相关

--- flutter_module/lib/main.dart
+++ flutter_module/lib/main.dart
@@ -1,44 +1,50 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // 导入 Material UI组件库
 import 'package:flutter/services.dart';
 
+// 应用入口，所以MyApp是应用的根组件
 void main() => runApp(MyApp());
 
+//void main() {
+//  runApp(MyApp()); 上面为单行函数的简写
+//}
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // widget的主要作用是提供一个build()方法来描述如何构建UI页面
   @override
   Widget build(BuildContext context) {
+    // MaterialApp 是Material库中提供的Flutter App 框架，可以设置应用名称、主题、语言、路由表等
     return MaterialApp(
+      // 应用名称
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in a Flutter IDE). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.lime,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page 777'),
+      // 名为"/"的路由作为应用的首页
+      initialRoute: '/',
+
+      // 路由表注册
+      routes: {
+        // 注册首页路由
+        "/":(context) => MyHomePage(title: 'Flutter Demo Home Page'),
+        "new_route":(context){
+          return NewRoute(text: ModalRoute.of(context).settings.arguments);
+        },
+      },
+//      home: MyHomePage(title: 'Flutter Demo Home Page 123'),
     );
   }
 }
 
+
+/*
+* StatefulWidget 至少由两个类组成
+* StatefulWidget 类
+* State 类
+* 注意 build 方法被绑到了 _MyHomePageState 中
+* */
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
@@ -47,9 +53,8 @@ class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
   void _incrementCounter() {
-//    SystemNavigator.pop();
-
     setState(() {
+      // 原注释
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
       // so that the display can reflect the updated values. If we changed
@@ -58,57 +63,16 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
       print(_counter);
     });
-//    Future.wait([
-//      // 2秒后返回结果
-//      Future.delayed(new Duration(seconds: 2), () {
-//        print("hello");
-//        return "hello";
-//      }),
-//      // 4秒后返回结果
-//      Future.delayed(new Duration(seconds: 4), () {
-//        print(" world");
-//        return " world";
-//      })
-//    ]).then((results){
-//      print(results[0]+results[1]);
-//    }).catchError((e){
-//      print(e);
-//    });
   }
-
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
@@ -121,15 +85,20 @@ class _MyHomePageState extends State<MyHomePage> {
             FlatButton(
               child: Text("open new route"),
               textColor: Colors.lightBlue,
-              onPressed: (){
-                Navigator.push(context,
-                  MaterialPageRoute(
-                    builder: (context){
-                      return NewRoute();
-                    },
-                    fullscreenDialog: false,
-                  )
-                );
+              onPressed: () async {
+                var result = await Navigator.pushNamed(context, "new_route",arguments: "hello");
+//                Navigator.push(
+//                    context,
+//                    MaterialPageRoute(
+//                      builder: (context){
+//                        return NewRoute(
+//                          text: "NewRoute",
+//                        );
+//                      },
+//                      fullscreenDialog: false,
+//                    )
+//                );
+                print("路由返回值：$result");
               },
             )
           ],
@@ -141,22 +110,39 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Increment',
         child: Icon(Icons.add_a_photo),
       ), // This trailing comma makes auto-formatting nicer for build methods.
-
     );
   }
 }
 
 class NewRoute extends StatelessWidget {
+  NewRoute({
+    Key key,
+    @required this.text // 接收 text 参数
+  }):super(key:key);
+  final String text;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    // Scaffold 为 Material库中提供的页面脚手架，包含导航栏和Body以及FloatingActionButton
     return Scaffold(
       appBar: AppBar(
         title:Text("New route"),
       ),
-      body: Center(
-        child: Text("This is new route"),
+      body: Padding(
+        padding: EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text("上个页面传过来的值：$text"),
+              RaisedButton(
+                onPressed: () => Navigator.pop(context,"New route"),
+                child: Text("返回"),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
 }
+
