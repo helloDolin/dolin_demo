a43dc3076f61013415b80d0c79a136bee9031403
liaoshaolim <366688603@qq.com>
2020-01-30 16:58:07 +0800

    1
    1

--- Pods/Headers/Public/FlutterPluginRegistrant/FlutterPluginRegistrant-umbrella.h
+++ Pods/Headers/Public/FlutterPluginRegistrant/FlutterPluginRegistrant-umbrella.h
@@ -1,1 +1,17 @@
-../../../Target Support Files/FlutterPluginRegistrant/FlutterPluginRegistrant-umbrella.h
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "GeneratedPluginRegistrant.h"
+
+FOUNDATION_EXPORT double FlutterPluginRegistrantVersionNumber;
+FOUNDATION_EXPORT const unsigned char FlutterPluginRegistrantVersionString[];
+
