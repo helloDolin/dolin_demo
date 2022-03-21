016980c732aebf263924c25437f945839fc0e236
helloDolin <366688603@qq.com>
2022-03-21 18:36:21 +0800

    1. dolin1data.json 格式化 2. 仿 Flutter SingleScrollView 实现 3. 删除 ScrollView Xib（改为手写） 4. 删除 AutoLayoutPriority，在自动布局 + ScrollView 中实现

--- dolin_demo/APP/Dolin1/AutoLayout+Masonry/AutoLayout_ScrollViewVC.h
+++ dolin_demo/APP/Dolin1/AutoLayout+Masonry/AutoLayout_ScrollViewVC.h
@@ -8,9 +8,16 @@
 
 #import <UIKit/UIKit.h>
 
+@interface ScrollBoxView : UIView
+
+@end
+
 /**
  如果每个cell都不同，就可以用这种方式，当然 table 也可以，算是 get 一个新技能吧
+ 类似 Flutter SingleScrollView
  */
 @interface AutoLayout_ScrollViewVC : UIViewController
 
 @end
+
+
--- dolin_demo/APP/Dolin1/AutoLayout+Masonry/AutoLayout_ScrollViewVC.m
+++ dolin_demo/APP/Dolin1/AutoLayout+Masonry/AutoLayout_ScrollViewVC.m
@@ -7,13 +7,77 @@
 //
 
 #import "AutoLayout_ScrollViewVC.h"
-#import "ScrollBoxView.h"
 
-@interface AutoLayout_ScrollViewVC ()
-{
-    NSArray* _arr;
+@interface ScrollBoxView ()
+
+@property (nonatomic, strong) UILabel *leftLbl;
+@property (nonatomic, strong) UILabel *rightLbl;
+
+@end
+
+@implementation ScrollBoxView : UIView
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self setupUI];
+    }
+    return self;
 }
 
+// 从 xib 实例化
+//+ (instancetype)viewFromXib {
+//    return [[[NSBundle mainBundle]loadNibNamed:NSStringFromClass([self class]) owner:nil options:nil]lastObject];
+//}
+
+- (void)setupUI {
+    [self addSubview:self.leftLbl];
+    [self addSubview:self.rightLbl];
+
+    [self.leftLbl mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.mas_offset(20);
+        make.centerY.equalTo(self);
+        make.right.equalTo(self.rightLbl.mas_left).offset(-10);
+    }];
+    
+    [self.rightLbl mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.right.mas_offset(-20);
+        make.centerY.equalTo(self);
+    }];
+    
+    // 默认：左边完全显示，右边超长 ...
+    // 优先级设置：左边... 右边完全显示
+    [self.leftLbl setContentCompressionResistancePriority:UILayoutPriorityDefaultLow forAxis:UILayoutConstraintAxisHorizontal];
+//    [self.rightLbl setContentCompressionResistancePriority:UILayoutPriorityDefaultHigh forAxis:UILayoutConstraintAxisHorizontal];
+}
+
+- (UILabel *)leftLbl {
+    if (!_leftLbl) {
+        _leftLbl = [[UILabel alloc] initWithFrame:CGRectZero];
+        _leftLbl.textAlignment = NSTextAlignmentLeft;
+        _leftLbl.textColor = [UIColor blackColor];
+        _leftLbl.font = [UIFont systemFontOfSize:18];
+        _leftLbl.text = @"test";
+    }
+    return  _leftLbl;
+}
+
+- (UILabel *)rightLbl {
+    if (!_rightLbl) {
+        _rightLbl = [[UILabel alloc] initWithFrame:CGRectZero];
+        _rightLbl.textAlignment = NSTextAlignmentLeft;
+        _rightLbl.textColor = [UIColor blackColor];
+        _rightLbl.font = [UIFont systemFontOfSize:18];
+        _rightLbl.text = @"test";
+    }
+    return  _rightLbl;
+}
+
+@end
+
+@interface AutoLayout_ScrollViewVC ()
+
+@property (nonatomic, copy) NSArray *arr;
 @property (nonatomic, strong) UIScrollView *scrollView;
 @property (nonatomic, strong) UIView *containerView;
 
@@ -26,62 +90,55 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    _arr = @[
-                @{@"name":@"照片",@"detail":@"照片aaa"},
-                @{@"name":@"卡片",@"detail":@"卡片aaa"},
-                @{@"name":@"钱包",@"detail":@"钱包aaa"},
-                @{@"name":@"A",@"detail":@""},
-                @{@"name":@"B",@"detail":@""},
-                @{@"name":@"C",@"detail":@""},
-                @{@"name":@"D",@"detail":@""},
-                @{@"name":@"E",@"detail":@""},
-                @{@"name":@"F",@"detail":@""},
-             ];
-    
-    [self layoutUI];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    
+    [self setupUI];
 }
 
 #pragma mark - method
-- (void)layoutUI {
+- (void)setupUI {
     self.view.backgroundColor = [UIColor whiteColor];
+    
     [self.view addSubview:self.scrollView];
     [self.scrollView addSubview:self.containerView];
+    
     [_scrollView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.edges.mas_equalTo(UIEdgeInsetsMake(NAVIGATION_BAR_HEIGHT, 0, 0, 0));
+        make.left.right.mas_offset(0);
+//        make.bottom.equalTo(self.view.mas_safeAreaLayoutGuideBottom);
+        make.center.equalTo(self.view);
+        make.height.greaterThanOrEqualTo(@300);
     }];
     
     [_containerView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.edges.equalTo(self->_scrollView);
-        make.width.equalTo(self->_scrollView.mas_width);
+        make.edges.equalTo(self.scrollView);
+        // 重点：宽度的约束一定要设置
+        make.width.equalTo(self.scrollView.mas_width);
     }];
     
     
     // 这个是精髓，记录新创建的View
     ScrollBoxView* nextBoxView = nil;
     
-    for (int i = 0; i < _arr.count; i++) {
-        ScrollBoxView* boxView = [ScrollBoxView viewFromXib];
-        boxView.name.text = _arr[i][@"name"];
-        boxView.detail.text = _arr[i][@"detail"];
+    for (int i = 0; i < self.arr.count; i++) {
+        ScrollBoxView* boxView = [[ScrollBoxView alloc]init];
+        boxView.backgroundColor = RANDOM_UICOLOR;
+        boxView.leftLbl.text = self.arr[i][@"name"];
+        boxView.rightLbl.text = self.arr[i][@"detail"];
+        
         [self.containerView addSubview:boxView];
+        
         [boxView mas_makeConstraints:^(MASConstraintMaker *make) {
-            make.left.right.equalTo(self->_containerView);
+            make.left.right.equalTo(self.containerView);
             make.height.equalTo(@ (30 + 30 * i));
             if (nextBoxView) {
                 make.top.mas_equalTo(nextBoxView.mas_bottom).offset(10);
             } else {
-                make.top.mas_equalTo(self->_containerView.mas_top).offset(10);
+                make.top.mas_equalTo(self.containerView.mas_top).offset(10);
             }
         }];
+        
         nextBoxView = boxView;
     }
-    // 这样设置自动约束，系统才能计算出 contentSize
-    [_containerView mas_makeConstraints:^(MASConstraintMaker *make) {
+    // 这样设置约束，系统才能计算出 contentSize
+    [self.containerView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.bottom.equalTo(nextBoxView.mas_bottom);
     }];
 }
@@ -101,4 +158,24 @@
     return _containerView;
 }
 
+- (NSArray *)arr {
+    if (!_arr) {
+        _arr = @[
+            @{@"name":@"照片照片照片照片照片照片照片照片照片照片照片照片照片照片",@"detail":@"照片aaa"},
+            @{@"name":@"卡片",@"detail":@"卡片aaa"},
+            @{@"name":@"钱包",@"detail":@"钱包aaa"},
+            @{@"name":@"A",@"detail":@""},
+            @{@"name":@"B",@"detail":@""},
+            @{@"name":@"C",@"detail":@""},
+            @{@"name":@"D",@"detail":@""},
+            @{@"name":@"E",@"detail":@""},
+            @{@"name":@"F",@"detail":@""},
+        ];
+    }
+    return _arr;
+}
+
 @end
+
+
+
